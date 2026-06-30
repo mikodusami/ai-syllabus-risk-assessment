6aad43bd3deb68ade61e3f18da983654bd125dc8
michaelodusami <186487435+michaelodusami@users.noreply.github.com>
2026-06-30 19:33:06 -0400

    refactor: restructure DebugInfo interface and add PreviewResponse and AppConfig interfaces

--- lib/types.ts
+++ lib/types.ts
@@ -1,5 +1,3 @@
-import { DebugInfo } from "next/dist/next-devtools/shared/types";
-
 export interface AnalysisResult {
   course: string;
   riskLevel: number; // changing this to a number, on a scale of 1-100 then will translate this into low / medium / high risk level or some other code, definitions of numbers will need to be handled, I believe if we do the number scale, it could be more accurate but we need to define this scale.
@@ -19,4 +17,44 @@ export interface UploadResponse {
   data?: AnalysisResult;
   error?: string;
   debug?: DebugInfo;
+}
+
+export interface DebugInfo {
+  fileName: string;
+  fileSize: number;
+  extractedChars: number;
+  estimatedTokens: number;
+  usedFallbackModel: boolean;
+  model: string | undefined;
+  durationMs: number;
+  extractedText?: string;
+  rawAiResponse?: string;
+  extractionTimeMs?: number;
+  apiCallTimeMs?: number;
+  strategy?: AnalysisStrategy;
+  chunksProcessed?: number;
+}
+
+export type AnalysisStrategy = "direct" | "chunked" | "summarize";
+
+export interface PreviewResponse {
+  success: boolean;
+  fileName: string;
+  fileSize: number;
+  extractedChars: number;
+  estimatedTokens: number;
+  exceedsLimit: boolean;
+  tokenLimit: number;
+  extractedText?: string;
+  error?: string;
+}
+
+export interface AppConfig {
+  mode: string;
+  models: string[];
+  defaultModel: string;
+  tokenLimits: {
+    nonStreaming: number;
+    maxFallback: number;
+  };
 }
