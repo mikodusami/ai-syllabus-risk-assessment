fe505f09e96e11789877b3a5febc0910b0c30e6f
michaelodusami <186487435+michaelodusami@users.noreply.github.com>
2026-06-30 15:30:07 -0400

    feat: add data interfaces and such to program

---
+++ lib/types.ts
@@ -0,0 +1,31 @@
+import { DebugInfo } from "next/dist/next-devtools/shared/types";
+
+export interface AnalysisResult {
+  course: string;
+  riskLevel: number; // changing this to a number, on a scale of 1-100 then will translate this into low / medium / high risk level or some other code, definitions of numbers will need to be handled, I believe if we do the number scale, it could be more accurate but we need to define this scale.
+  issues: RiskIssue[];
+  summary: string;
+}
+
+export interface RiskIssue {
+  title: string;
+  description: string;
+  risk: string;
+  recommendation: string;
+}
+
+export interface UploadResponse {
+  success: boolean;
+  data?: AnalysisResult;
+  error?: string;
+  debug?: DebugInfo;
+}
+
+// const riskLevelRangeDefinition = [
+//   (0, "<some-meaning>"),
+//   (20, "<some-meaning>"),
+//   (40, "<some-meaning>"),
+//   (60, "<some-meaning>"),
+//   (80, "<some-meaning>"),
+//   (100, "<some-meaning>"),
+// ];
